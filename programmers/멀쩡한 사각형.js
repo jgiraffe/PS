43045fd54050364f062504351869b10567711622
Jaegyun Jung <jgiraffe@naver.com>
2020-10-26 02:23:44 +0900

    Update 멀쩡한 사각형.js

--- programmers/멀쩡한 사각형.js
+++ programmers/멀쩡한 사각형.js
@@ -1,2 +1,4 @@
-const gcd = (a, b) => b == 0 ? a : gcd(b, a % b);
-const solution = (w, h) => w * h - (w + h - gcd(w, h));
+let l=(_,$)=>$?l($,_%$):_, solution=(_,$)=>_*$-(_+$-l(_,$))
+
+// const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+// const solution = (w, h) => w * h - (w + h - gcd(w, h));
