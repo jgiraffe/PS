b01121fc30632cc4ed13efbe6577997c75d6685f
Jaegyun Jung <jgiraffe@naver.com>
2020-12-17 23:58:45 +0900

    Update 두 개 뽑아서 더하기.cpp

--- programmers/Level1/두 개 뽑아서 더하기.cpp
+++ programmers/Level1/두 개 뽑아서 더하기.cpp
@@ -1,21 +1,15 @@
 #include <vector>
-#include <algorithm>
+#include <set>
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
     vector<int> answer;
-    int size = numbers.size();
-    for(int i = 0; i < size; i++) {
-        for(int j = i + 1; j < size; j++) {
-            answer.push_back(numbers[i]+numbers[j]);
+    set<int> answer_set;
+    for(int i = 0; i < numbers.size(); i++) {
+        for(int j = i+1; j < numbers.size(); j++) {
+            answer_set.insert(numbers[i]+numbers[j]);
         }
     }
-    sort(answer.begin(), answer.end());
-    for(int i = 0; i < answer.size() - 1; i++) {
-        if(answer[i] == answer[i+1]) {
-            answer.erase(answer.begin()+i+1);
-            i-=1;
-        }
-    }
+    answer.assign(answer_set.begin(), answer_set.end());
     return answer;
 }
