183770351f7c95e9cf7258dfb4adade8767b54b1
Jaegyun Jung <jgiraffe@naver.com>
2020-12-18 17:14:19 +0900

    Create 문자열 다루기 기본.cpp

---
+++ programmers/Level1/문자열 다루기 기본.cpp
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if(s.length() != 4 && s.length() != 6) return false;
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] < '0' || s[i] > '9') return false;
+    }
+    return answer;
+}
