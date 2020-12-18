cacbe0524b1c322d4886bb59d6e49c72ead22502
Jaegyun Jung <jgiraffe@naver.com>
2020-12-18 22:18:14 +0900

    Create 문자열을 정수로 바꾸기.cpp

---
+++ programmers/Level1/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+
+int solution(string s) {
+    int answer = 0, n = 1;
+    for(int i = s.length()-1; i > 0; i--) {
+        answer += (s[i]-'0') * n;
+        n*=10;
+    }
+    if(s[0] == '+') return answer;
+    else if(s[0] == '-') return answer * -1;
+    else return answer + (s[0]-'0') * n;
+}
