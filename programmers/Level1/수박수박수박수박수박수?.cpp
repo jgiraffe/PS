b74d628ae5730820d3b1edddad03cff54fe71b65
Jaegyun Jung <jgiraffe@naver.com>
2020-12-18 22:10:52 +0900

    Create 수박수박수박수박수박수?.cpp

---
+++ programmers/Level1/수박수박수박수박수박수?.cpp
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for(int i = 0; i < n/2; i++) {
+        answer += "수박";
+    }
+    if(n%2) return answer + "수";
+    return answer;
+}
